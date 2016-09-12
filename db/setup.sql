9443e847feb8b8acefa894fa70cdb971c466d72c
louismerlin <louis.merlin@epfl.ch>
2016-09-12 18:34:04 +0200

    Create tables for most objects

--- db/setup.sql
+++ db/setup.sql
@@ -1,4 +1,28 @@
-create table hohoho(
-	hohoho_id serial primary key,
-	user_name varchar(16) not null,
+create table users (
+	sciper integer primary key,
+	firstname varchar(16) not null,
+	lastname varchar(16) not null,
+	email text not null unique,
+	description integer[]
+);
+
+create table questions (
+	date_asked date not null,
+	question_body text,
+	answers text[]
+);
+
+create table lunches (
+	lunch_id serial primary key,
+	date date not null
+);
+
+create table lunch_user (
+	user_id integer references users,
+	lunch_id integer references lunches
+);
+
+create table subjects (
+	subject_id serial primary key,
+	body text not null
 );
