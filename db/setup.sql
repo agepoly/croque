5318ddd72c991c8658c1ddd9aad030f734b98fc8
Nansse <gauthier@de-chezelles.com>
2016-09-13 18:09:43 +0200

    Update of the db
    We restricted ourself to one language, one question/day and no subject (the daily question is the subject).
    Questions are not linked to a specific day so they can easily be reused (table daily).
    Answers are linked to a specific day so you can have different answers to the same question

--- db/setup.sql
+++ db/setup.sql
@@ -5,24 +5,61 @@ create table users (
 	email text not null unique,
 	description integer[]
 );
+create index user_index on users (sciper)
 
 create table questions (
-	date_asked date not null,
+	id integer primary key,
 	question_body text,
 	answers text[]
 );
+create index question_index on questions (id)
 
-create table lunches (
-	lunch_id serial primary key,
-	date date not null
+create table daily (
+	date_asked date primary key,
+	question_id, integer not null,
+	
+	foreign key (question_id) references questions(id)
 );
+create index daily_index on daily (date_asked)
 
-create table lunch_user (
-	user_id integer references users,
-	lunch_id integer references lunches
+create answers (
+	date_asked date,
+	user_id integer,
+	answer varchar(200) not null,
+	
+	foreign key (date_asked) references daily(date_asked),
+	foreign key (user_id) references users(id),
+	primary key (date_asked, user_id)
 );
+create index answers_index on answers (date_asked, user_id)
 
-create table subjects (
-	subject_id serial primary key,
-	body text not null
+create table lunch_groups (
+
+	id serial primary key,
+	date date not null;
+	user_id_1 integer not null,
+	user_id_2 integer not null,
+	user_id_3 integer,
+	user_id_4 integer,
+	
+	foreign key (date) references daily (date_asked),
+	foreign key (user_id_1) references questions (id),
+	foreign key (user_id_2) references questions (id),
+	foreign key (user_id_3) references questions (id),
+	foreign key (user_id_4) references questions (id),
+	
 );
+# Maybe we should create an index on each usersid as we will often be searching through them 
+
+create table messages (
+	id serial primary key,
+	lunch_group_id integer not null,
+	time date not null,
+	expeditor_id integer not null,
+	message text,
+	
+	foreign key (lunch_group_id) references lunch_groups (id),
+	foreign key (expeditor_id) references users (id),
+	
+);
+create index messages_index on messages (lunch_group_id)
