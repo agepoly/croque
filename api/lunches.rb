9dc06bb48f394ace08d6864bcfaada7274fae13b
Louis Merlin <louis.merlin@epfl.ch>
2016-12-12 11:02:49 +0100

    Add error messages if time is bad in request creation

--- api/lunches.rb
+++ api/lunches.rb
@@ -14,7 +14,12 @@ class LunchApp < Sinatra::Base
     @data = JSON.parse(request.body.read)
     @time = @data['time']
     @answers = @data['answers']
-    if @time != nil && @answers != nil && @answers != [] && !Lunchrequest.where(user_id: session[:logged]).first && !User[session[:logged]].lunches_dataset.where(date: Date.today).first
+		if @time == nil
+			halt 400, 'time must not be empty'
+		elsif !([11, 12, 13].include? @time.to_i)
+			halt 400, 'time must be 11, 12 or 13'
+		end
+		if @answers != nil && @answers != [] && !Lunchrequest.where(user_id: session[:logged]).first && !User[session[:logged]].lunches_dataset.where(date: Date.today).first
       @lr = Lunchrequest.new(time: @time.to_i, user_id: session[:logged]).save
       @answers.each {|a|
         if Answer[a]
@@ -25,6 +30,12 @@ class LunchApp < Sinatra::Base
     else
       400
     end
+  end
+
+  get '/lunches' do
+    protected!
+    @lunches = User[session[:logged]].lunches_dataset.all
+    @lunches.to_json
   end
 
   # Setting basic options
