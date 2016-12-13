afad759f6ed08997cd2b24ea1a6c33e890fcdb89
Louis Merlin <louis.merlin@epfl.ch>
2016-12-13 10:26:13 +0100

    Refactor and comment the algo part

--- admin/algo.rb
+++ admin/algo.rb
@@ -5,6 +5,7 @@ def distribute
   @prop.each do |p|
     @sum += p.proportion
   end
+  # Default distribution if this one is not valid
   if @sum.zero?
     @prop = [{ size: 2, proportion: 1 }, { size: 3, proportion: 1 }] # As many 2s than 3s
     @sum = 2
@@ -15,59 +16,47 @@ def distribute
   @portions = 1
   @reqs = Lunchrequest.all
 
+  # Find all tables for a specific time
+  def findTable(time)
+    @exact = @prop.index { |p| p.size == Lunchrequest.where(time: time).all.size }
+    if !@exact.nil?
+      createLunch(@prop[@exact][:proportion], time)
+    else
+      @rand = rand(@sum)
+      @te = 0
+      @count = 0
+      while @rand > @te
+        @te += @prop[@count][:proportion]
+        @count += 1
+      end
+      createLunch(@prop[@count].size, time)
+    end
+  end
+
+  # Create a lunch for num persons at the specified time
+  def createLunch(num, time)
+    log = Logger.new(STDERR)
+    log.fatal(num)
+    log.fatal(time)
+
+    @timeReqs = Lunchrequest.where(time: time).all
+    @chosen = @timeReqs.first(num)
+    @lunch = Lunch.new(time: time, date: Date.today).save
+    @chosen.each do |req|
+      @lunch.add_user(req[:user_id])
+      req.delete
+    end
+  end
+
+
   @times = [11, 12, 13]
 
+  # Create tables for each time slot
   for @time in @times do
     findTable(@time) while Lunchrequest.where(time: @time).all.size >= @small
-end
-
-  # @newLunches = []
-  # @newLunches = endTest(@newLunches, @small)
-
-  # DELETE ALL REQUESTS
-end
-
-def findTable(time)
-  @reqs = Lunchrequest.all
-  @small = @prop[0].size
-  @prop = Distribution.where { (proportion > 0) }.all.select { |d| (d.size < @reqs.size) && (d.size > 1) }
-  @sum = 0
-  @prop.each do |p|
-    @sum += p.proportion
-  end
-  if @sum.zero?
-    @prop = [{ size: 2, proportion: 1 }, { size: 3, proportion: 1 }] # As many 2s than 3s
-    @sum = 2
-  end
-  @prop.each { |p| @small = p.size if p.size < @small }
-  @exact = @prop.index { |p| p.size == Lunchrequest.where(time: time).all.size }
-  if !@exact.nil?
-    createLunch(@prop[@exact][:proportion], time)
-  else
-    @rand = rand(@sum)
-    @te = 0
-    @count = 0
-    while @rand > @te
-      @te += @prop[@count][:proportion]
-      @count += 1
-    end
-    createLunch(@prop[@count].size, time)
   end
 end
 
-def createLunch(num, time)
-  log = Logger.new(STDERR)
-  log.fatal(num)
-  log.fatal(time)
-
-  @reqs = Lunchrequest.where(time: time).all
-  @chosen = @reqs.first(num)
-  @lunch = Lunch.new(time: time, date: Date.today).save
-  @chosen.each do |req|
-    @lunch.add_user(req[:user_id])
-    req.delete
-  end
-end
 
 scheduler = Rufus::Scheduler.new
 
